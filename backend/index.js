a58bf7e719cd4d5ed71e75f9255b53e731c02495
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2024-05-07 09:52:31 +0200

    WIP

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // const express = require('express'),
 //   path = require('path')
-const express = requires('express');
-const app = express()
+const express = require('express');
+const app = express();
 
 // Import
 const blogRoutes = require('./routes/blogRoutes');
